8af20eec4b08917861be7693a6fe38eaea29cd9b
Rajath <rajathkrishna.jagana@dish.com>
2020-06-07 00:59:28 +0530

    Initial commit

---
+++ assets/js/carousel.js
@@ -0,0 +1,9 @@
+$("#news-slider2").owlCarousel({
+    items:3,
+    itemsDesktop:[1199,2],
+    itemsDesktopSmall:[980,2],
+    itemsMobile:[600,1],
+    pagination:true,
+    navigationText:false,
+    autoPlay:true
+});
